7bbdca7ff6625037971adcb5c7adf9f500f0c768
Krrish Agarwalla <121277606+kkkamur07@users.noreply.github.com>
2026-09-09 19:01:58 +0530

    Solve probstat-percentiles

---
+++ study-plans/math-probability/probstat-percentiles/probstat-percentiles.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def percentiles(x, q):
+    """
+    Returns: numpy array of percentile values.
+    """
+    pass
+
+    x = np.array(x)
+    q = np.array(q)
+
+    return np.percentile(x,q)
+        
